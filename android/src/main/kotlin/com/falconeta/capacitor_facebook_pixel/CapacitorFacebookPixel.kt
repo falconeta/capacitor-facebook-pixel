6c156c26ec3159a1ed9c62a4d17f5fe02c1c2f08
Vittorio Lo Preiato <vittorio.lopreiato@freedompro.eu>
2022-10-28 14:16:40 +0200

    implements facebook sdk for android

--- android/src/main/kotlin/com/falconeta/capacitor_facebook_pixel/CapacitorFacebookPixel.kt
+++ android/src/main/kotlin/com/falconeta/capacitor_facebook_pixel/CapacitorFacebookPixel.kt
@@ -1,8 +1,55 @@
 package com.falconeta.capacitor_facebook_pixel
 
+import android.content.Context
+import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsConstants.EVENT_PARAM_CONTENT
+import com.facebook.appevents.AppEventsConstants.EVENT_PARAM_CONTENT_TYPE
+import com.facebook.appevents.AppEventsConstants.EVENT_PARAM_CURRENCY
+import com.facebook.appevents.AppEventsConstants.EVENT_NAME_PURCHASED
+import com.facebook.appevents.AppEventsLogger
+import com.getcapacitor.Bridge
+
 
 class CapacitorFacebookPixel {
+
+  private var webView: WebView
+  private var context: Context
+
+  private var logger: AppEventsLogger
+
+  constructor(bridge: Bridge) {
+    webView = bridge.webView
+    context = bridge.context
+    logger = AppEventsLogger.newLogger(context)
+
+//    AppEventsLogger.augmentWebView(webView,context)
+
+    // for enable or disable first time
+    FacebookSdk.setAutoInitEnabled(true)
+    FacebookSdk.fullyInitialize()
+
+    FacebookSdk.setAutoLogAppEventsEnabled(true)
+
+
+//    val params = Bundle()
+//    params.putString(EVENT_PARAM_CURRENCY, "USD")
+//    params.putString(EVENT_PARAM_CONTENT_TYPE, "product")
+//    params.putString(
+//      EVENT_PARAM_CONTENT,
+//      "[{\"id\": \"1234\", \"quantity\": 2}, {\"id\": \"5678\", \"quantity\": 1}]"
+//    )
+
+//    logger.logEvent(
+//      EVENT_NAME_PURCHASED,
+//      54.23,
+//      params
+//    )
+    logger.logEvent("BattleTheMonster");
+  }
+
     fun echo(value: String): String {
         Log.i("Echo", value)
         return value
--- android/src/main/kotlin/com/falconeta/capacitor_facebook_pixel/CapacitorFacebookPixelPlugin.kt
+++ android/src/main/kotlin/com/falconeta/capacitor_facebook_pixel/CapacitorFacebookPixelPlugin.kt
@@ -1,19 +1,21 @@
 package com.falconeta.capacitor_facebook_pixel
 
-import com.getcapacitor.JSObject
-import com.getcapacitor.Plugin
-import com.getcapacitor.PluginCall
-import com.getcapacitor.PluginMethod
+import com.getcapacitor.*
 import com.getcapacitor.annotation.CapacitorPlugin
 
 @CapacitorPlugin(name = "CapacitorFacebookPixel")
 class CapacitorFacebookPixelPlugin : Plugin() {
-    private val implementation: CapacitorFacebookPixel = CapacitorFacebookPixel()
+  private lateinit var  implementation: CapacitorFacebookPixel
+  override fun load() {
+    super.load()
+     implementation = CapacitorFacebookPixel(bridge)
+  }
+
     @PluginMethod
     fun echo(call: PluginCall) {
-        val value: String = call.getString("value")
+        val value: String? = call.getString("value")
         val ret = JSObject()
-        ret.put("value", implementation.echo(value))
+        ret.put("value", value?.let { implementation.echo(it) })
         call.resolve(ret)
     }
 }
